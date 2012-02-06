b1bb36be9c6e7d0eadad8d71c3263c8e7259df11
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-06 20:35:41 +0800

    update to new liblights by Stefan Seidel

--- Android.mk
+++ Android.mk
@@ -5,13 +5,10 @@ LOCAL_PATH := $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := lights.$(TARGET_PRODUCT)
+LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := lights.c
+LOCAL_MODULE := lights.default
 
-ifeq ($(strip $(TARGET_PRODUCT)),s103t)
-LOCAL_SRC_FILES := s103t_lights.c
 include $(BUILD_SHARED_LIBRARY)
-endif
